256cbf4d5fc98c0b4f14d2f704a6395a6062a3d4
Marina Elizarova <seagirl1110@gmail.com>
2024-08-08 11:59:29 +0300

    feat: add ImageComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import ImageComponent from './components/ImageComponent';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ImageComponent />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ImageComponent.jsx
@@ -0,0 +1,10 @@
+function ImageComponent() {
+  return (
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWestySFdjEYa_HB1RMZVgx07ds7WXNUpLaQ&usqp=CAU"
+      alt="cat-image"
+    />
+  );
+}
+
+export default ImageComponent;
