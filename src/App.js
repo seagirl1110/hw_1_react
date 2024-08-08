22e3a9bc8e0dd3b476218bf05dd065c28e247d98
Marina Elizarova <seagirl1110@gmail.com>
2024-08-08 16:46:39 +0300

    feat: add ParagraphsComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ImageComponent from './components/ImageComponent';
 import VideoComponent from './components/VideoComponent';
+import ParagraphsComponent from './components/ParagraphsComponent';
 
 function App() {
   return (
     <div className="App">
       <ImageComponent />
       <VideoComponent />
+      <ParagraphsComponent />
     </div>
   );
 }
---
+++ src/components/ParagraphsComponent.jsx
@@ -0,0 +1,35 @@
+function ParagraphsComponent() {
+  return (
+    <div>
+      <article>
+        <h3>Lorem, ipsum.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          dignissimos rerum similique modi ut quis incidunt illum vero magnam
+          possimus!
+        </p>
+      </article>
+      <article>
+        <h3>Lorem, ipsum dolor.</h3>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+          natus iusto debitis inventore obcaecati nihil ullam totam sint
+          distinctio, at, dolorum accusamus, nesciunt magnam eligendi ducimus
+          quis minus. Voluptatum, accusantium?{' '}
+        </p>
+      </article>
+      <article>
+        <h3>Lorem ipsum dolor sit.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
+          eveniet molestias distinctio obcaecati commodi ipsam. Illo sint non
+          sequi corrupti cumque, quia fugit veniam obcaecati eaque dolore
+          eligendi porro maxime facere pariatur consequatur incidunt, nesciunt,
+          odio labore eius. Minus, asperiores!{' '}
+        </p>
+      </article>
+    </div>
+  );
+}
+
+export default ParagraphsComponent;
