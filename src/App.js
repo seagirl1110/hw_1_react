debfe55bab3bb957c34616f1eea40142c95c846e
Marina Elizarova <seagirl1110@gmail.com>
2024-08-08 16:39:29 +0300

    feat: add VideoComponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ImageComponent from './components/ImageComponent';
+import VideoComponent from './components/VideoComponent';
 
 function App() {
   return (
     <div className="App">
       <ImageComponent />
+      <VideoComponent />
     </div>
   );
 }
---
+++ src/components/VideoComponent.jsx
@@ -0,0 +1,5 @@
+function VideoComponent() {
+  return <video src="#" width="500" height="300" controls="controls" />;
+}
+
+export default VideoComponent;
